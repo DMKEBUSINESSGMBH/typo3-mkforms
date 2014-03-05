a05926e64825d5b36916bac64e83b8533e335198
Hannes Bochmann <hannes.bochmann@dmk-ebusiness.de>
2014-03-05 17:06:00 +0100

    php error gefixed

--- api/class.majixmethods.php
+++ api/class.majixmethods.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once(t3lib_extMgm::extPath('mkforms') . 'api/class.inlineconfmethods.php'');
+require_once(t3lib_extMgm::extPath('mkforms') . 'api/class.inlineconfmethods.php');
 
 class formidable_majixmethods extends formidable_inlineconfmethods {
 }
