f826296c9edd4d97d40f3df901a0c0e2fe88b7ed
René Nitzsche <nitzsche@das-medienkombinat.de>
2014-11-04 18:39:58 +0100

    Column name fixed for T3 6.2

--- ext_tables.sql
+++ ext_tables.sql
@@ -3,23 +3,24 @@
 # Table for mkforms session cache
 #
 CREATE TABLE cf_mkforms (
-	id int(11) NOT NULL auto_increment,
-	identifier varchar(128) DEFAULT '' NOT NULL,
+	id int(11) unsigned NOT NULL auto_increment,
+	identifier varchar(250) DEFAULT '' NOT NULL,
 	crdate int(11) DEFAULT '0' NOT NULL,
 	content longblob NOT NULL,
+	expires int(11) DEFAULT '0' NOT NULL,
 	lifetime int(11) DEFAULT '0' NOT NULL,
 
 	PRIMARY KEY (id),
-	KEY cache_id (identifier)
+	KEY cache_id (identifier,expires)
 );
 
 #
 # Unused dummy table for TYPO3 caching framework
 #
 CREATE TABLE cf_mkforms_tags (
-	id int(11) NOT NULL auto_increment,
-	identifier varchar(128) DEFAULT '' NOT NULL,
-	tag varchar(128) DEFAULT '' NOT NULL,
+	id int(11) unsigned NOT NULL auto_increment,
+	identifier varchar(250) DEFAULT '' NOT NULL,
+	tag varchar(250) DEFAULT '' NOT NULL,
 
 	PRIMARY KEY (id),
 	KEY cache_id (identifier),
