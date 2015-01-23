b97d8bb863d0f92294f8328eb974325ad774a7cd
Hannes Bochmann <hannes.bochmann@dmk-ebusiness.de>
2015-01-23 14:19:09 +0100

    fehlender load

--- ext_localconf.php
+++ ext_localconf.php
@@ -5,6 +5,7 @@
 	require_once(t3lib_extMgm::extPath('rn_base') . 'class.tx_rnbase.php');
 	tx_rnbase::load('tx_mkforms_util_Div');
 	tx_rnbase::load('tx_mkforms_util_Constants');
+	tx_rnbase::load('tx_rnbase_configurations');
 
 	if(t3lib_extMgm::isLoaded('mksanitizedparameters')){
 		require_once(t3lib_extMgm::extPath($_EXTKEY).'ext_rules.php');
