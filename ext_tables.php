dfeb15e148df378437f95d1b454cc29b2821b66d
Oliver Klee <typo3-coding@oliverklee.de>
2015-06-18 20:37:10 +0200

    [CLEANUP] Autoformat the PHP files in the extension root
    This has been done using the PhpStorm code formatter.

--- ext_tables.php
+++ ext_tables.php
@@ -1,28 +1,30 @@
 <?php
 
-if (!defined ('TYPO3_MODE'))     die ('Access denied.');
+if (!defined('TYPO3_MODE')) {
+	die ('Access denied.');
+}
 
 ////////////////////////////////
 // Plugin anmelden
 ////////////////////////////////
 // Einige Felder ausblenden
-$TCA['tt_content']['types']['list']['subtypes_excludelist']['tx_mkforms']='layout,select_key,pages';
+$TCA['tt_content']['types']['list']['subtypes_excludelist']['tx_mkforms'] = 'layout,select_key,pages';
 
 // Das tt_content-Feld pi_flexform einblenden
 $TCA['tt_content']['types']['list']['subtypes_addlist']['tx_mkforms'] = 'pi_flexform';
 
-t3lib_extMgm::addPiFlexFormValue('tx_mkforms','FILE:EXT:'.$_EXTKEY.'/flexform_main.xml');
+t3lib_extMgm::addPiFlexFormValue('tx_mkforms', 'FILE:EXT:' . $_EXTKEY . '/flexform_main.xml');
 
 t3lib_extMgm::addPlugin(
-		array(
-			'LLL:EXT:'.$_EXTKEY.'/locallang_db.php:plugin.mkforms.label',
-			'tx_mkforms',
-			t3lib_extMgm::extRelPath($_EXTKEY) . 'ext_icon.gif',
-		)
-	);
+	array(
+		'LLL:EXT:' . $_EXTKEY . '/locallang_db.php:plugin.mkforms.label',
+		'tx_mkforms',
+		t3lib_extMgm::extRelPath($_EXTKEY) . 'ext_icon.gif',
+	)
+);
 
-t3lib_extMgm::addStaticFile($_EXTKEY,'static/ts/', 'MKFORMS - Basics');
-t3lib_extMgm::addStaticFile($_EXTKEY,'static/prototype/', 'MKFORMS Prototype-JS');
-t3lib_extMgm::addStaticFile($_EXTKEY,'static/jquery/', 'MKFORMS JQuery-JS');
+t3lib_extMgm::addStaticFile($_EXTKEY, 'static/ts/', 'MKFORMS - Basics');
+t3lib_extMgm::addStaticFile($_EXTKEY, 'static/prototype/', 'MKFORMS Prototype-JS');
+t3lib_extMgm::addStaticFile($_EXTKEY, 'static/jquery/', 'MKFORMS JQuery-JS');
 
 ?>
