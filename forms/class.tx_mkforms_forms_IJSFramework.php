287f9ad48f9e359df06e7c759776fdc916d376de
Oliver Klee <typo3-coding@oliverklee.de>
2015-07-09 14:29:59 +0200

    [CLEANUP] Autoformat the PHP files in forms/

--- forms/class.tx_mkforms_forms_IJSFramework.php
+++ forms/class.tx_mkforms_forms_IJSFramework.php
@@ -26,17 +26,22 @@ require_once(t3lib_extMgm::extPath('mkforms') . 'api/class.tx_ameosformidable.ph
 
 /**
  * Factory for forms.
- *
  */
 interface tx_mkforms_forms_IJSFramework {
 	public function getBaseIncludes();
+
 	public function includeBase();
+
 	public function getEffectIncludes();
+
 	public function includeTooltips();
+
 	public function includeDragDrop();
+
 	/**
 	 * Liefert den Namen des JSFrameworks: jquery, prototype usw.
 	 * Dieser Wert wird von Widgets verwendet um zusätzliche Dateien abhängig vom aktuellen Framework einzubinden.
+	 *
 	 * @return string
 	 */
 	public function getId();
@@ -44,25 +49,48 @@ interface tx_mkforms_forms_IJSFramework {
 
 interface tx_mkforms_forms_IPageInclude {
 	public function getPagePath();
+
 	public function getServerPath();
+
 	public function getKey();
+
 	/**
 	 * Bei einem JS-Incluce true, bei CSS false
+	 *
 	 * @return string
 	 */
 	public function isJS();
+
 	public function isFirstPos();
+
 	public function getBeforeKey();
+
 	public function getAfterKey();
 }
 
 class tx_mkforms_forms_PageInclude implements tx_mkforms_forms_IPageInclude {
+
 	private $pagePath, $serverPath, $key, $firstPos, $beforeKey, $afterKey, $isJS;
 
-	public static function createInstance($pagePath, $serverPath='', $sKey = '', $isJS=true, $bFirstPos = FALSE, $sBefore = '', $sAfter = '') {
+	public static function createInstance(
+		$pagePath,
+		$serverPath = '',
+		$sKey = '',
+		$isJS = TRUE,
+		$bFirstPos = FALSE,
+		$sBefore = '',
+		$sAfter = '') {
 		return new tx_mkforms_forms_PageInclude($pagePath, $serverPath, $sKey, $isJS, $bFirstPos, $sBefore, $sAfter);
 	}
-	public function __construct($pagePath, $serverPath='', $sKey = '', $isJS=true, $bFirstPos = FALSE, $sBefore = '', $sAfter = '') {
+
+	public function __construct(
+		$pagePath,
+		$serverPath = '',
+		$sKey = '',
+		$isJS = TRUE,
+		$bFirstPos = FALSE,
+		$sBefore = '',
+		$sAfter = '') {
 		$this->pagePath = $pagePath;
 		$this->serverPath = $serverPath;
 		$this->key = $sKey;
@@ -75,27 +103,35 @@ class tx_mkforms_forms_PageInclude implements tx_mkforms_forms_IPageInclude {
 	public function getPagePath() {
 		return $this->pagePath;
 	}
+
 	public function getServerPath() {
 		return $this->serverPath;
 	}
+
 	public function getKey() {
 		return $this->key;
 	}
+
 	public function isFirstPos() {
 		return $this->firstPos;
 	}
+
 	public function getBeforeKey() {
 		return $this->beforeKey;
 	}
+
 	public function getAfterKey() {
 		return $this->afterKey;
 	}
+
 	public function isJS() {
 		return $this->isJS;
 	}
 }
 
-if (defined('TYPO3_MODE') && $GLOBALS['TYPO3_CONF_VARS'][TYPO3_MODE]['XCLASS']['ext/mkforms/forms/class.tx_mkforms_forms_IJSFramework.php']) {
+if (defined('TYPO3_MODE')
+	&& $GLOBALS['TYPO3_CONF_VARS'][TYPO3_MODE]['XCLASS']['ext/mkforms/forms/class.tx_mkforms_forms_IJSFramework.php']
+) {
 	include_once($GLOBALS['TYPO3_CONF_VARS'][TYPO3_MODE]['XCLASS']['ext/mkforms/forms/class.tx_mkforms_forms_IJSFramework.php']);
 }
 ?>
