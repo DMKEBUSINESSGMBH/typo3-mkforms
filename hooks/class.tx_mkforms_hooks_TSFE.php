a4a4ab39889e3bc014e488025f068b4436b8e961
Oliver Klee <typo3-coding@oliverklee.de>
2015-07-09 14:30:35 +0200

    [CLEANUP] Autoformat the PHP files in hooks/

--- hooks/class.tx_mkforms_hooks_TSFE.php
+++ hooks/class.tx_mkforms_hooks_TSFE.php
@@ -2,10 +2,10 @@
 
 class tx_mkforms_hooks_TSFE {
 	function contentPostProc_output() {
-		if(isset($GLOBALS['tx_ameosformidable']) && isset($GLOBALS['tx_ameosformidable']['headerinjection'])) {
+		if (isset($GLOBALS['tx_ameosformidable']) && isset($GLOBALS['tx_ameosformidable']['headerinjection'])) {
 
 			reset($GLOBALS['tx_ameosformidable']['headerinjection']);
-			while(list(, $aHeaderSet) = each($GLOBALS['tx_ameosformidable']['headerinjection'])) {
+			while (list(, $aHeaderSet) = each($GLOBALS['tx_ameosformidable']['headerinjection'])) {
 				$GLOBALS['TSFE']->content = str_replace(
 					$aHeaderSet['marker'],
 					implode("\n", $aHeaderSet['headers']) . "\n" . $aHeaderSet['marker'],
@@ -16,7 +16,9 @@ class tx_mkforms_hooks_TSFE {
 	}
 }
 
-if (defined('TYPO3_MODE') && $GLOBALS['TYPO3_CONF_VARS'][TYPO3_MODE]['XCLASS']['ext/mkforms/hooks/class.tx_mkforms_hooks_TSFE.php'])	{
+if (defined('TYPO3_MODE')
+	&& $GLOBALS['TYPO3_CONF_VARS'][TYPO3_MODE]['XCLASS']['ext/mkforms/hooks/class.tx_mkforms_hooks_TSFE.php']
+) {
 	include_once($GLOBALS['TYPO3_CONF_VARS'][TYPO3_MODE]['XCLASS']['ext/mkforms/hooks/class.tx_mkforms_hooks_TSFE.php']);
 }
 
