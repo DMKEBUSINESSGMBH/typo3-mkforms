d7e7690579fa8de69882e42a11015aded39c309c
Michael Wagner <michael.wagner@dmk-ebusiness.de>
2016-09-28 09:39:38 +0200

    Cleanup php code for hidden widget

--- widgets/hidden/class.tx_mkforms_widgets_hidden_Main.php
+++ widgets/hidden/class.tx_mkforms_widgets_hidden_Main.php
@@ -6,33 +6,51 @@
  */
 
 
-class tx_mkforms_widgets_hidden_Main extends formidable_mainrenderlet {
+class tx_mkforms_widgets_hidden_Main
+	extends formidable_mainrenderlet
+{
 
-	function _render() {
+	/**
+	 * {@inheritDoc}
+	 * @see formidable_mainrenderlet::_render()
+	 */
+	public function _render()
+	{
+		$value = $this->getValue();
 
-		$sValue = $this->getValue();
-		$sValueForHtml = $this->getValueForHtml($sValue);
-
-		$sInput = "<input type=\"hidden\" name=\"" . $this->_getElementHtmlName() . "\" id=\"" . $this->_getElementHtmlId() . "\" value=\"" . $sValueForHtml . "\"" . $this->_getAddInputParams() . " />";
+		$inputHtml = '<input ' .
+			'type="hidden" ' .
+			'name="' . $this->_getElementHtmlName() . '" ' .
+			'id="' . $this->_getElementHtmlId() . '" ' .
+			'value="' . $this->getValueForHtml($value) . '"' .
+			$this->_getAddInputParams() .
+		' />';
 
 		return array(
-			"__compiled" => $sInput,
-			"input" => $sInput,
-			"value" => $sValue,
+			"__compiled" => $inputHtml,
+			"input" => $inputHtml,
+			"value" => $value,
 		);
 	}
 
-	function _renderReadonly() {
+	/**
+	 * {@inheritDoc}
+	 * @see formidable_mainrenderlet::_renderReadonly()
+	 */
+	public function _renderReadonly()
+	{
 		return $this->_render();
 	}
 
-	function _activeListable() {
+	/**
+	 * {@inheritDoc}
+	 * @see formidable_mainrenderlet::_activeListable()
+	 */
+	public function _activeListable() {
 		return TRUE;
 	}
 }
 
-
-	if (defined("TYPO3_MODE") && $GLOBALS['TYPO3_CONF_VARS'][TYPO3_MODE]["XCLASS"]["ext/ameos_formidable/api/base/rdt_hidden/api/class.tx_rdthidden.php"])	{
-		include_once($GLOBALS['TYPO3_CONF_VARS'][TYPO3_MODE]["XCLASS"]["ext/ameos_formidable/api/base/rdt_hidden/api/class.tx_rdthidden.php"]);
-	}
-
+if (defined("TYPO3_MODE") && $GLOBALS['TYPO3_CONF_VARS'][TYPO3_MODE]["XCLASS"]["ext/ameos_formidable/api/base/rdt_hidden/api/class.tx_rdthidden.php"]) {
+	include_once($GLOBALS['TYPO3_CONF_VARS'][TYPO3_MODE]["XCLASS"]["ext/ameos_formidable/api/base/rdt_hidden/api/class.tx_rdthidden.php"]);
+}
