3a3efaf93448049b463c8a747ac24ada22e88d4b
Michael Wagner <michael.wagner@dmk-ebusiness.de>
2017-03-13 10:59:19 +0100

    some phpcs fixes and getMainSubpart is public again

--- view/class.tx_mkforms_view_Form.php
+++ view/class.tx_mkforms_view_Form.php
@@ -1,34 +1,27 @@
 <?php
-/**
- * 	@package tx_mkforms
- *  @subpackage tx_mkforms_view
- *  @author Michael Wagner
+/***************************************************************
+ * Copyright notice
  *
- *  Copyright notice
+ * (c) 2011 - 2017 DMK E-BUSINESS GmbH <dev@dmk-ebusiness.de>
+ * All rights reserved
  *
- *  (c) 2011 Michael Wagner <dev@dmk-business.de>
- *  All rights reserved
+ * This script is part of the TYPO3 project. The TYPO3 project is
+ * free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  *
- *  This script is part of the TYPO3 project. The TYPO3 project is
- *  free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
+ * The GNU General Public License can be found at
+ * http://www.gnu.org/copyleft/gpl.html.
  *
- *  The GNU General Public License can be found at
- *  http://www.gnu.org/copyleft/gpl.html.
+ * This script is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- *  This script is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  This copyright notice MUST APPEAR in all copies of the script!
- */
+ * This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
 
-/**
- * benötigte Klassen einbinden
- */
 tx_rnbase::load('tx_rnbase_view_Base');
 tx_rnbase::load('tx_rnbase_util_Link');
 tx_rnbase::load('tx_rnbase_util_Templates');
@@ -40,7 +33,8 @@ tx_rnbase::load('tx_rnbase_util_Templates');
  * @subpackage tx_mkforms_view
  * @author Michael Wagner
  */
-class tx_mkforms_view_Form extends tx_rnbase_view_Base {
+class tx_mkforms_view_Form extends tx_rnbase_view_Base
+{
 
 	/**
 	 * Do the output rendering.
@@ -49,10 +43,13 @@ class tx_mkforms_view_Form extends tx_rnbase_view_Base {
 	 * @param 	ArrayObject					$viewData
 	 * @param 	tx_rnbase_configurations	$configurations
 	 * @param 	tx_rnbase_util_FormatUtil	$formatter
+	 *
 	 * @return 	mixed						Ready rendered output or HTTP redirect
 	 */
+	// @codingStandardsIgnoreStart (interface/abstract mistake)
 	public function createOutput($template, &$viewData, &$configurations, &$formatter, $redirectToLogin = false) {
-		// Wrnn konfiguriert, einen redirekt nach erfolgreichem absenden der form durchführen
+		// @codingStandardsIgnoreEnd
+		// Winn konfiguriert, einen redirekt nach erfolgreichem absenden der form durchführen
 		$this->handleRedirect($viewData, $configurations);
 
 		$confId = $this->getController()->getConfId();
@@ -69,8 +66,8 @@ class tx_mkforms_view_Form extends tx_rnbase_view_Base {
 				$markerArrays = $subpartArrays  = $wrappedSubpartArrays = array();
 
 				foreach ($data as $key => $values) {
-					$currentMarkerPrefix = strtoupper($key).'_';
-					$currentConfId = $confId.$key.'.';
+					$currentMarkerPrefix = strtoupper($key) . '_';
+					$currentConfId = $confId . $key . '.';
 					/*
 					 * @TODO: bei den Values sollte man auch Objekte übergeben können und die
 					 * Markerklasse wird konfiguriert
@@ -83,25 +80,33 @@ class tx_mkforms_view_Form extends tx_rnbase_view_Base {
 					 * müssen für den speziellen Fall die add Methoden
 					 * oder die globale addAdditionalMarkers Methode angelegt werden!
 					 */
-					if(tx_rnbase_util_BaseMarker::containsMarker($template, $currentMarkerPrefix)) {
+					if (tx_rnbase_util_BaseMarker::containsMarker($template, $currentMarkerPrefix)) {
 						$currentSubpartArray = $currentWrappedSubpartArray = array();
 						$currentMarkerArray = $formatter->getItemMarkerArrayWrapped(
-								$values, $currentConfId, 0, $currentMarkerPrefix, null);
+							$values,
+							$currentConfId,
+							0,
+							$currentMarkerPrefix,
+							null
+						);
 						// wir suchen für jede Tabelle eine parse Methode in der Kindklasse!
-						$method = 'add'.tx_mkforms_util_Div::toCamelCase($key).'Markers';
-						if(method_exists($this, $method)) {
+						$method = 'add' . tx_mkforms_util_Div::toCamelCase($key) . 'Markers';
+						if (method_exists($this, $method)) {
 							$template = $this->{$method}(
-									$values, $currentMarkerPrefix,
-									$currentMarkerArray, $currentSubpartArray, $currentWrappedSubpartArray,
-									$currentConfId, $formatter, $template, $viewData
-								);
+								$values, $currentMarkerPrefix,
+								$currentMarkerArray, $currentSubpartArray, $currentWrappedSubpartArray,
+								$currentConfId, $formatter, $template, $viewData
+							);
 						}
-						if(!empty($currentMarkerArray))
+						if (!empty($currentMarkerArray)) {
 							$markerArrays[] = $currentMarkerArray;
-						if(!empty($currentSubpartArray))
+						}
+						if (!empty($currentSubpartArray)) {
 							$subpartArrays[] = $currentSubpartArray;
-						if(!empty($currentWrappedSubpartArray))
+						}
+						if (!empty($currentWrappedSubpartArray)) {
 							$wrappedSubpartArrays[] = $currentWrappedSubpartArray;
+						}
 					}
 				}
 				// die marker arrays zusammenführen
@@ -113,18 +118,29 @@ class tx_mkforms_view_Form extends tx_rnbase_view_Base {
 					: call_user_func_array('array_merge', $wrappedSubpartArrays);
 			}
 		}
-		//
-		$template = $this->addAdditionalMarkers($data, 'DATA',
-			$markerArray, $subpartArray, $wrappedSubpartArray,
-			$confId, $formatter, $template, $viewData
+
+		$template = $this->addAdditionalMarkers(
+			$data,
+			'DATA',
+			$markerArray,
+			$subpartArray,
+			$wrappedSubpartArray,
+			$confId,
+			$formatter,
+			$template,
+			$viewData
 		);
 
 		$markerArray['###FORM###'] = $viewData->offsetGet('form');
 		$out = tx_rnbase_util_Templates::substituteMarkerArrayCached(
-				$template, $markerArray, $subpartArray, $wrappedSubpartArray);
+			$template,
+			$markerArray,
+			$subpartArray,
+			$wrappedSubpartArray
+		);
 
 		// Fehler ausgeben, wenn gesetzt.
-		if(strlen($errors = $viewData->offsetGet('errors')) > 0){
+		if (strlen($errors = $viewData->offsetGet('errors')) > 0) {
 			$out .= $errors;
 		}
 
@@ -141,25 +157,41 @@ class tx_mkforms_view_Form extends tx_rnbase_view_Base {
 	 * @param 	array 						$wrappedSubpartArray
 	 * @param 	tx_rnbase_util_FormatUtil 	$formatter
 	 * @param 	string 						$template
+	 *
 	 * @return 	string
 	 */
+	// @codingStandardsIgnoreStart (interface/abstract mistake)
 	protected function addAdditionalMarkers(
-			$data, $markerPrefix,
-			&$markerArray, &$subpartArray, &$wrappedSubpartArray,
-			$confId, &$formatter, $template, $viewData) {
-
+		$data,
+		$markerPrefix,
+		&$markerArray,
+		&$subpartArray,
+		&$wrappedSubpartArray,
+		$confId,
+		&$formatter,
+		$template,
+		$viewData
+	) {
+		// @codingStandardsIgnoreEnd
 
 		// links parsen
 		// @TODO: über rnbase simple marker parsen?
-		$linkIds = $formatter->getConfigurations()->getKeyNames($confId.'links.');
-		foreach($linkIds as $linkId) {
-			$params = $formatter->getConfigurations()->get($confId.'links.'.$linkId.'.params.');
+		$linkIds = $formatter->getConfigurations()->getKeyNames($confId . 'links.');
+		foreach ($linkIds as $linkId) {
+			$params = $formatter->getConfigurations()->get(
+				$confId . 'links.' . $linkId . '.params.'
+			);
 			tx_rnbase_util_BaseMarker::initLink(
-					$markerArray, $subpartArray, $wrappedSubpartArray,
-					$formatter, $confId, $linkId, $markerPrefix,
-						empty($params) ? array() : $params,
-					$template
-				);
+				$markerArray,
+				$subpartArray,
+				$wrappedSubpartArray,
+				$formatter,
+				$confId,
+				$linkId,
+				$markerPrefix,
+				empty($params) ? array() : $params,
+				$template
+			);
 		}
 
 		return $template;
@@ -171,21 +203,26 @@ class tx_mkforms_view_Form extends tx_rnbase_view_Base {
 	 *
 	 * @param 	ArrayObject 				$viewData
 	 * @param 	tx_rnbase_configurations 	$configurations
+	 *
 	 * @return 	void
 	 */
-	protected function handleRedirect(&$viewData, &$configurations) {
+	// @codingStandardsIgnoreStart (interface/abstract mistake)
+	protected function handleRedirect(&$viewData, &$configurations)
+	{
+	// @codingStandardsIgnoreEnd
+
 		$confId = $this->getController()->getConfId();
-		if (
+		if ((
 			// redirect if fully submitted
 			$viewData->offsetGet('fullySubmitted')
 			// if there are no validation errors
 			&& !$viewData->offsetGet('hasValidationErrors')
 			// and redirect configured
 			&& (
-				$configurations->getBool($confId.'redirect') ||
-				$configurations->get($confId.'redirect.pid')
+				$configurations->getBool($confId . 'redirect') ||
+				$configurations->get($confId . 'redirect.pid')
 			)
-		) {
+		)) {
 			// Speichern wir die Sessiondaten vor dem Redirect? Die würden sonst verloren gehen!
 			$GLOBALS['TSFE']->fe_user->storeSessionData();
 
@@ -195,13 +232,18 @@ class tx_mkforms_view_Form extends tx_rnbase_view_Base {
 			exit('REDIRECTED!');
 		}
 	}
+
 	/**
 	 * Erzeugt den Link für den Redirect. Kind-Klassen haben die Möglchkeit diese Methode zu überschreiben.
+	 *
 	 * @param ArrayObject $viewData
 	 * @param tx_rnbase_configurations $configurations
 	 * @param string $confId
 	 */
-	protected function createRedirectLink($viewData, $configurations, $confId) {
+	// @codingStandardsIgnoreStart (interface/abstract mistake)
+	protected function createRedirectLink($viewData, $configurations, $confId)
+	{
+		// @codingStandardsIgnoreStart (interface/abstract mistake)
 		$params	= $viewData->offsetGet('redirect_parameters');
 		$link = $configurations->createLink();
 		$link->initByTS($configurations, $confId.'redirect.', is_array($params) ? $params : array());
@@ -214,7 +256,7 @@ class tx_mkforms_view_Form extends tx_rnbase_view_Base {
 	 *
 	 * @return string
 	 */
-	function getMainSubpart(&$viewData) {
+	public function getMainSubpart(&$viewData) {
 		tx_rnbase::load('tx_rnbase_action_BaseIOC');
 		$controller = $this->getController();
 		if($controller instanceof tx_rnbase_action_BaseIOC) {
