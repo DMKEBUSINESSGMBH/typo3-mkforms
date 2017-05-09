07a37b56012a3fd4d31e74d116387e577713f83c
Michael Wagner <michael.wagner@dmk-ebusiness.de>
2017-05-09 08:33:18 +0200

    add typo3 8.7 to allowed require

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,14 +1,14 @@
 <?php
 
-// 
+//
 // Extension Manager/Repository config file for ext: "mkforms"
-// 
+//
 // Auto generated 09-03-2008 22:19
-// 
+//
 // Manual updates:
 // Only the data in the array - anything else is removed by next write.
 // "version" and "dependencies" must not be touched!
-// 
+//
 
 $EM_CONF[$_EXTKEY] = array(
     'title' => 'MK Forms',
@@ -35,7 +35,7 @@ $EM_CONF[$_EXTKEY] = array(
     'constraints' => array(
         'depends' => array(
             'rn_base' => '1.0.9-',
-            'typo3' => '4.5.0-7.6.99',
+            'typo3' => '4.5.0-8.7.99',
         ),
         'conflicts' => array(
             'ameos_formidable' => ''
