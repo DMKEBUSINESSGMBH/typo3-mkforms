f7cc6de698aaf77f04acf0544f2446503c3a5e17
Hannes Bochmann <hannes.bochmann@dmk-ebusiness.com>
2019-07-25 17:08:03 +0200

    bugfix fixed name of checkbox
    example how it was before:
    name of hidden field: ...[hidden_field]
    name of checkbox field: ...[hidden_field][checkbox]
    
    The request will have the parameter for the hidden field removed as
    the value of the checkbox makes an array out of the hidden field parameter
    
    example how it is now:
    name of hidden field: ...[hidden_field]
    name of checkbox field: ...[hidden_field_checkbox]

--- widgets/checksingle/class.tx_mkforms_widgets_checksingle_Main.php
+++ widgets/checksingle/class.tx_mkforms_widgets_checksingle_Main.php
@@ -39,8 +39,8 @@ class tx_mkforms_widgets_checksingle_Main extends formidable_mainrenderlet
                 $this->_getAddInputParams()
             );
             $sInput .= sprintf(
-                '<input type="checkbox" name="%1$s[checkbox]" id="%2$s_checkbox" %3$s %4$s value="1" />',
-                $this->_getElementHtmlName(),
+                '<input type="checkbox" name="%1$s" id="%2$s_checkbox" %3$s %4$s value="1" />',
+                str_replace($this->getName(), $this->getName().'_checkbox', $this->_getElementHtmlName()),
                 $this->_getElementHtmlId(),
                 $sChecked,
                 $this->_getAddInputParams()
