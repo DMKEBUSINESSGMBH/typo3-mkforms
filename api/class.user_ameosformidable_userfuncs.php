e988a337a38fdba0da87aa1806047a8166a1210f
Hannes Bochmann <hannes.bochmann@dmk-ebusiness.com>
2019-08-19 16:16:18 +0200

    fixed coding style
    #92

--- api/class.user_ameosformidable_userfuncs.php
+++ api/class.user_ameosformidable_userfuncs.php
@@ -21,5 +21,5 @@ class user_ameosformidable_userfuncs
 if (defined('TYPO3_MODE')
     && $GLOBALS['TYPO3_CONF_VARS'][TYPO3_MODE]['XCLASS']['ext/ameos_formidable/api/class.user_ameosformidable_userfuncs.php']
 ) {
-    include_once($GLOBALS['TYPO3_CONF_VARS'][TYPO3_MODE]['XCLASS']['ext/ameos_formidable/api/class.user_ameosformidable_userfuncs.php']);
+    include_once $GLOBALS['TYPO3_CONF_VARS'][TYPO3_MODE]['XCLASS']['ext/ameos_formidable/api/class.user_ameosformidable_userfuncs.php'];
 }
