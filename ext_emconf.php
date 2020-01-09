7dc82bb21f6bd082ed7e7f0d23c5f5cd72bf3f14
Michael Wagner <michael.wagner@dmk-ebusiness.de>
2020-01-09 07:53:27 +0100

    Drop support for php 5.4 and php 5.5

--- ext_emconf.php
+++ ext_emconf.php
@@ -35,7 +35,7 @@ $EM_CONF[$_EXTKEY] = array(
         'depends' => array(
             'rn_base' => '1.10.5-',
             'typo3' => '7.6.0-9.5.99',
-            'php' => '5.4.0-8.9.99',
+            'php' => '5.6.0-8.9.99',
         ),
         'conflicts' => array(
             'ameos_formidable' => '',
