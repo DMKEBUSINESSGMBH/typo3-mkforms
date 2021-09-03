1f53f601c02e6207be940c93ad39d6eb6ae73de8
Hannes Bochmann <hannes.bochmann@dmk-ebusiness.com>
2021-09-03 09:59:48 +0200

    Bugfix add middleware after TS initialization

--- Configuration/RequestMiddlewares.php
+++ Configuration/RequestMiddlewares.php
@@ -5,11 +5,10 @@ return [
         'dmk/mkforms/ajax-handler' => [
             'target' => \DMK\MkForms\Middleware\AjaxHandler::class,
             'after' => [
-                'typo3/cms-frontend/tsfe',
+                'typo3/cms-frontend/prepare-tsfe-rendering',
             ],
             'before' => [
                 'typo3/cms-frontend/output-compression',
-                'typo3/cms-frontend/prepare-tsfe-rendering',
             ],
         ],
     ],
